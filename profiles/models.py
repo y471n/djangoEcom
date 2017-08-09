4cd1fbc990c4f782f8034e881619538e896b9331
yatin <iamyatin@gmail.com>
2017-08-09 16:01:42 +0530

    Add a home page route

--- profiles/models.py
+++ profiles/models.py
@@ -5,5 +5,5 @@ class Profile(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(default='')
 
-    def __repr__(self):
+    def __str__(self):
         return self.name
