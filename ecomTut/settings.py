7835c8e268f346b1d9bd4565a78a45e7523e5ecd
yatin <iamyatin@gmail.com>
2017-08-09 18:26:25 +0530

    Create template system

--- ecomTut/settings.py
+++ ecomTut/settings.py
@@ -121,4 +121,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+if DEBUG:
+    MEDIA_URL = 'media/'
+    STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static",
+                               "static-only")
+    MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static", "media")
+    STATICFILES_DIR = (
+        os.path.join(os.path.dirname(BASE_DIR), "static", "static")
+    )
+
 # TEMPLATE_DIRS =
--- ecomTut/urls.py
+++ ecomTut/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import url
 from django.contrib import admin
 
@@ -22,3 +24,9 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_ROOT,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_ROOT,
+                          document_root=settings.MEDIA_ROOT)
