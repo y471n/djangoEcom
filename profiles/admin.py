58c48effe0524e27a30c0ce54d242b6c2e2f1580
yatin <iamyatin@gmail.com>
2017-08-09 15:31:30 +0530

    Register profile with Admin

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Profile
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Profile
+
+admin.site.register(Profile, ProfileAdmin)
