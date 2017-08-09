b40736cf195de6bf3bb65e138631e01573e808ff
yatin <iamyatin@gmail.com>
2017-08-09 18:30:56 +0530

    Register an about page

--- profiles/views.py
+++ profiles/views.py
@@ -5,3 +5,9 @@ def home(request):
     context = locals()
     template = 'home.html'
     return render(request, template, context=context)
+
+
+def about(request):
+    context = locals()
+    template = 'about.html'
+    return render(request=request, template_name=template, context=context)
